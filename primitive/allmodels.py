a038564b1a952b26cb24f29cae6be17f261a9ad4
Michiel van der Ree <michiel.van.der.ree@target-holding.nl>
2015-10-31 13:20:18 +0100

    classifier skeleton update

--- primitive/allmodels.py
+++ primitive/allmodels.py
@@ -1,19 +1,24 @@
 __author__ = 'coenjonker'
 
 from functions import *
+import numpy as np
 
 class WeightedNearestNeighbor():
     def __init__(self, week_weights, classes, names):
         self.week_weights = week_weights
         self.classes_ = classes
+        expected_names = [u'Id', u'Dates', u'DayOfWeek', u'PdDistrict', u'Address', u'X', u'Y']
         self.names = names
+        if not np.all(np.in1d(expected_names, names)):
+            raise Exception("Not all expected variables supplied")
+
 
     def fit(self, X, y):
         self.X_memory = X
         self.y_memory = y
 
     def predict(self, X):
-        #magic
+
         pass
 
 
