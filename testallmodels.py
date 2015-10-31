b739359b8b59bcdfb5393eb1d2b4fd46486377b0
coenjonker <coen.j.jonker@gmail.com>
2015-10-31 16:10:03 +0100

    restructure directories

--- testallmodels.py
+++ testallmodels.py
@@ -1,15 +1,25 @@
 __author__ = 'coenjonker'
 
-import inspect
-
-from primitive import allmodels
+import sys
+from coen_models import MostLikelyClassPerRegion
+from load_data import load_data
 
 #TODO: Iets slims met numpy arrays om efficient alle modellen te evalueren volgens logloss functie
 # We kunnen dit ook lekker laten zitten en KAggle laten evalueren, maar we hebben max 5 inzendingen per dag, dus als we gaan
 # Monte Carlo optimaliseren is het wellicht handiger dit ding wel ff te implementeren :)
-if __name__ == __main__:
+if __name__ == "__main__":
+    train_f = sys.argv[1]
+    test_f = sys.argv[2]
 
-    models = [name for name, obj in inspect.getmembers(allmodels) if inspect.isclass(obj)]
+    train_data = load_data(train_f)
+    test_data = load_data(test_f)
+
+    model = MostLikelyClassPerRegion()
+
+    model.fit(train_data[0:200])
+    small_test = test_data[0:10]
+    print model.predict_proba(small_test.values)
+
 
 
 
