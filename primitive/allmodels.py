b3d25493479f59597995cc78a8b3097dd6cc2cb4
Michiel van der Ree <michiel.van.der.ree@target-holding.nl>
2015-10-31 13:11:35 +0100

    classifier skeleton

--- primitive/allmodels.py
+++ primitive/allmodels.py
@@ -1,3 +1,20 @@
 __author__ = 'coenjonker'
 
+from functions import *
+
+class WeightedNearestNeighbor():
+    def __init__(self, week_weights, classes, names):
+        self.week_weights = week_weights
+        self.classes_ = classes
+        self.names = names
+
+    def fit(self, X, y):
+        self.X_memory = X
+        self.y_memory = y
+
+    def predict(self, X):
+        #magic
+        pass
+
+
 
