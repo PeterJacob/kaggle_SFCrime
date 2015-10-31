eca594280cbf39573446d593a41bb486f7f8d3cd
Peter Smit <mail@peterjacobsmit.com>
2015-10-31 20:42:03 +0100

    Changes

--- test.py
+++ test.py
@@ -2,6 +2,10 @@ import numpy as np
 import sklearn.dummy
 from PredictionMachine import PredictionMachine
 
-dtree = sklearn.dummy.DummyClassifier(strategy='most_frequent')
-PM = PredictionMachine(dtree)
+features_path = '/home/peter/Documents/kaggle_SFCrime/data/train.npy'
+labels_path   = '/home/peter/Documents/kaggle_SFCrime/data/labels.npy'
+
+classifier = sklearn.dummy.DummyClassifier(strategy='most_frequent')
+PM = PredictionMachine(classifier, 
+    features_path=features_path, labels_path=labels_path)
 PM.run()
