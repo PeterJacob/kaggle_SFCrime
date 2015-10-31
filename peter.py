8b8e06f6ef3c2c086d84f58f10182040e1a17363
Peter Smit <mail@peterjacobsmit.com>
2015-10-31 14:25:05 +0100

    Fixed typos

--- peter.py
+++ peter.py
@@ -1,11 +1,12 @@
-def create_folds(weeks=None, n_iter=5, train_size=0.5, test_size=0.5):
+def create_folds(strat_col=None, n_iter=5, train_size=0.5, test_size=0.5):
     # Create cross-validation shizzle
     from sklearn.cross_validation import StratifiedShuffleSplit
    
-    if weeks in None:
-        weeks = [1,2,3,1,2,3]
-   
-    cf = StratifiedShuffleSplit(weeks, n_iter=n_iter, 
+    if strat_col is None:
+        strat_col = [1,2,3,1,2,3]
+        print('No strat_col given. Using: {}'.format(str(strat_col)))
+    
+    cf = StratifiedShuffleSplit(strat_col, n_iter=n_iter, 
                                 train_size=train_size, test_size=test_size)
     
     return cf
