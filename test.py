637e53d3b9e6cecc204fe3f2950cd22a33ed1ebf
coenjonker <coen.j.jonker@gmail.com>
2015-10-31 20:40:23 +0100

    Prepper fixed

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ import numpy as np
 import sklearn.dummy
 from PredictionMachine import PredictionMachine
 
-dtree = sklearn.dummy.DummyClassifier(strategy='most_frequent')
+#dtree = sklearn.dummy.DummyClassifier(strategy='most_frequent')
+model =
 PM = PredictionMachine(dtree)
 PM.run()
