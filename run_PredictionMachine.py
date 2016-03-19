426ddde16eb249ed4d12b342574170f594aed2ac
Peter Smit <mail@peterjacobsmit.com>
2016-03-19 12:28:41 +0100

    Added example script for PredictionMachine

---
+++ run_PredictionMachine.py
@@ -0,0 +1,11 @@
+import sklearn.dummy
+from PredictionMachine import PredictionMachine
+
+
+features_path = '/home/peter/Documents/kaggle_SFCrime/data/train.npy'
+labels_path   = '/home/peter/Documents/kaggle_SFCrime/data/labels.npy'
+
+
+classifier = sklearn.dummy.DummyClassifier(strategy='most_frequent')
+PM = PredictionMachine(classifier, features_path=features_path, labels_path=labels_path)
+PM.run()
